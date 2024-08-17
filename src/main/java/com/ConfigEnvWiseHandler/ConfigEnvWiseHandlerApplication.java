baca5c94b958e6de57beb393c68625e66b293c49
vrittiksharma <vrittiksharma2@gmail.com>
2024-08-17 23:33:34 +0530

    initial commit

---
+++ src/main/java/com/ConfigEnvWiseHandler/ConfigEnvWiseHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.ConfigEnvWiseHandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigEnvWiseHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigEnvWiseHandlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ConfigEnvWiseHandler/ConfigEnvWiseHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ConfigEnvWiseHandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigEnvWiseHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
